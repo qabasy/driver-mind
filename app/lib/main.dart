1653071f25adcab82adba6ba3a40418af776dffa
qabasy <mahros.work@hotmail.com>
2024-09-17 10:43:10 +0300

    Latest-Update

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,11 +1,10 @@
 // import 'dart:io';
 
 import 'package:app/src/app.dart';
-// import 'package:app/src/models/database_model.dart';
 import 'package:flutter/material.dart';
-// import 'package:sqflite_common_ffi/sqflite_ffi.dart' as sqflite_common_ffi;
+import 'package:permission_handler/permission_handler.dart';
 
-void main(List<String> args) {
+void main(List<String> args) async {
   // if (Platform.isWindows) {
   //   // Initialize FFI
   //   sqflite_common_ffi.sqfliteFfiInit();
@@ -13,5 +12,13 @@ void main(List<String> args) {
   //   sqflite_common_ffi.databaseFactory = sqflite_common_ffi.databaseFactoryFfi;
   // }
 
+  await Permission.manageExternalStorage.request();
+  await Permission.systemAlertWindow.request();
+  await Permission.criticalAlerts.request();
   runApp(const DriverMindApp());
+  // await Permission.storage.request().then((value) {
+  //   if (!(value == PermissionStatus.denied ||
+  //       value == PermissionStatus.permanentlyDenied)) {
+  // }
+  // });
 }
